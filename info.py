08bce83be19e3f022f5145e7cddc5f60463885ed
Shreeraam23 <161189756+Shreeraam23@users.noreply.github.com>
2025-08-30 16:43:06 +0530

    Update info.py

--- info.py
+++ info.py
@@ -12,9 +12,9 @@ BOT_TOKEN = environ.get('BOT_TOKEN', '')  # Telegram Bot Token
 
 # 👑, Channels & Logs
 BIN_CHANNEL = int(environ.get("BIN_CHANNEL", '-1003042522134'))  # File storage channel
-LOG_CHANNEL = int(environ.get("LOG_CHANNEL", '-1003042522134'))  # General log channel
-PREMIUM_LOGS = int(environ.get("PREMIUM_LOGS", '-1003042522134'))  # Premium user actions log
-VERIFIED_LOG = int(environ.get('VERIFIED_LOG', '-1003042522134'))  # Verified user actions log
+LOG_CHANNEL = int(environ.get("LOG_CHANNEL", '-1002732217390'))  # General log channel
+PREMIUM_LOGS = int(environ.get("PREMIUM_LOGS", '-1002732217390'))  # Premium user actions log
+VERIFIED_LOG = int(environ.get('VERIFIED_LOG', '-1002732217390'))  # Verified user actions log
 SUPPORT_GROUP = int(environ.get("SUPPORT_GROUP", "-1002413032052"))
 
 # add admin IDs 11111 2222 3333 and add auth channel IDs -100XXX -100XXX -100XXX
@@ -22,7 +22,7 @@ ADMINS = list(map(int, environ.get('ADMINS', '8184060262').split()))  # List of 
 AUTH_CHANNEL = list(map(int, environ.get("AUTH_CHANNEL", "-1002413032052").split()))  # Allowed channels for authorization
 
 # username add without @
-OWNER_USERNAME = environ.get("OWNER_USERNAME", 'sheetu479')  # Owner's username
+OWNER_USERNAME = environ.get("OWNER_USERNAME", 'Pre_contact_bot')  # Owner's username
 BOT_USERNAME = environ.get("BOT_USERNAME", 'Filetolinkstreamerbot')  # Bot's username
 
 # 🔗 Channel & Support Links
